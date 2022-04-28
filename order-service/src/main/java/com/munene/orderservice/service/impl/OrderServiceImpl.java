0e833367360b25b9c9fe22019662e25434952d16
Moses Munene <muthawamunene@gmail.com>
2022-04-28 12:57:32 +0300

    adds order payment processing

--- order-service/src/main/java/com/munene/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/munene/orderservice/service/impl/OrderServiceImpl.java
@@ -1,12 +1,14 @@
 package com.munene.orderservice.service.impl;
 
 import com.munene.dto.OrderDto;
+import com.munene.dto.PaymentDto;
 import com.munene.orderservice.domain.Order;
 import com.munene.orderservice.domain.OrderStatus;
 import com.munene.orderservice.repository.OrderRepository;
 import com.munene.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -14,20 +16,37 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private RestTemplate paymentRestTemplate;
+
+    private String PAYMENT_ENDPOINT_URL = "http://localhost:8083/payments";
+
     @Override
     public OrderDto create(OrderDto orderDto) {
         Order order = new Order();
         order.setUserId(orderDto.getUserDto().getId());
         order.setAmount(orderDto.getAmount());
         order.setOrderStatus(OrderStatus.NEW);
+
         order = this.create(order);
         orderDto.setId(order.getId());
+        orderDto.setStatus(order.getOrderStatus().toString());
+
         return orderDto;
     }
 
     @Override
     public Order create(Order order) {
+
         order = this.orderRepository.save(order);
+        PaymentDto paymentDto = new PaymentDto();
+        paymentDto.setOrderId(order.getId());
+        paymentDto.setAmount(order.getAmount());
+        paymentDto = paymentRestTemplate.postForObject(PAYMENT_ENDPOINT_URL, paymentDto, PaymentDto.class);
+
+        order.setOrderStatus(paymentDto.getStatus().equals("success") ? OrderStatus.COMPLETED : OrderStatus.DECLINED);
+
+        this.orderRepository.save(order);
         return order;
     }
 }
