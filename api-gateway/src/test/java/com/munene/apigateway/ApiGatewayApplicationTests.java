7fc0f8985d5c3c24394ec5dcbf7a27064bf3d13e
Moses Munene <muthawamunene@gmail.com>
2022-04-29 10:21:25 +0300

    Adds Hystrix Dashboard

---
+++ api-gateway/src/test/java/com/munene/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munene.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
